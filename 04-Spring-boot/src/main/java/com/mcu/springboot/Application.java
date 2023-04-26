5a176c1b753216ea7952796c17017f38e38ee295
Mohacel <118649370+mohacelhosen@users.noreply.github.com>
2023-04-26 15:49:47 +0600

    SpringApplication.run() method explainig

---
+++ 04-Spring-boot/src/main/java/com/mcu/springboot/Application.java
@@ -0,0 +1,15 @@
+package com.mcu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+        System.out.println(context.getClass().getName());
+        System.out.println("Beans Loaded:: "+ context.getBeanDefinitionCount());
+    }
+
+}
---
+++ 04-Spring-boot/src/main/java/com/mcu/springboot/CacheManager.java
@@ -0,0 +1,13 @@
+package com.mcu.springboot;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CacheManager implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Logic executing to load data into cache...");
+    }
+}
